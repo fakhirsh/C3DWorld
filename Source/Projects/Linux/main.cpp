33e89734fda65b7251b880df6659ee9c1e82a6b4
Fakhir Shaheen <fakhir.shaheen@gmail.com>
2017-11-10 22:13:07 +0500

    Input implemented: Keyboard + Mouse + Scroll

--- Source/Projects/Linux/main.cpp
+++ Source/Projects/Linux/main.cpp
@@ -5,9 +5,14 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include "../../FEngine/Renderer/GL30/SimpleVertex2DProgram.hpp"
+//#include "../../FEngine/Renderer/GL30/SimpleVertex2DProgram.hpp"
 
 GLFWwindow * window;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 int main( void ){
 	// Initialise GLFW
@@ -45,20 +50,24 @@ int main( void ){
 
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetScrollCallback(window, scroll_callback);
 
 	// Dark blue background
-	glClearColor(1.0f, 0.0f, 0.4f, 0.0f);
+	glClearColor(1.0f, 0.0f, 0.4f, 1.0f);
 
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
+//	GLuint VertexArrayID;
+//	glGenVertexArrays(1, &VertexArrayID);
+//	glBindVertexArray(VertexArrayID);
 
 
-    FEngine::SimpleVertex2DProgram program;
-    if(!program.Init()){
-        std::cout << "ERROR: Couldn't initialize shader" << std::endl;
-        return 1;
-    }
+//    FEngine::SimpleVertex2DProgram program;
+//    if(!program.Init()){
+//        std::cout << "ERROR: Couldn't initialize shader" << std::endl;
+//        return 1;
+//    }
 
 
 	static const GLfloat g_vertex_buffer_data[] = {
@@ -67,16 +76,16 @@ int main( void ){
 		 0.0f,  1.0f, 0.0f,
 	};
 
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+//	GLuint vertexbuffer;
+//	glGenBuffers(1, &vertexbuffer);
+//	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
 	do{
 
 		// Clear the screen
 		glClear( GL_COLOR_BUFFER_BIT );
-
+/*
 		program.Bind();
         //program.SetMatrix(640, 480);
 		// 1rst attribute buffer : vertices
@@ -95,6 +104,7 @@ int main( void ){
 		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
 
 		glDisableVertexAttribArray(0);
+*/
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -105,13 +115,40 @@ int main( void ){
 		   glfwWindowShouldClose(window) == 0 );
 
 	// Cleanup VBO
-	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteVertexArrays(1, &VertexArrayID);
+//	glDeleteBuffers(1, &vertexbuffer);
+//	glDeleteVertexArrays(1, &VertexArrayID);
 
-	program.UnLoad();
+//	program.UnLoad();
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
 
 	return 0;
+}
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_E && action == GLFW_PRESS)
+        std::cout << "Keyboard E pressed" <<std::endl;
+}
+
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    std::cout << "x: " << xpos << " - y: " << ypos << std::endl;
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+        std::cout << "Mouse Right button pressed" <<std::endl;
+
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+        std::cout << "Mouse Left button pressed" <<std::endl;
+
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    std::cout << "xOff: " << xoffset << " - yOff: " << yoffset << std::endl;
 }
